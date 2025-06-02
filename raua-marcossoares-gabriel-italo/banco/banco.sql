ccf6cd7c91506078fdcfc081c7093d17b775fd3b
AlunoFodido <00001132718521sp@al.educacao.sp.gov.br>
2025-06-02 09:31:47 -0300

    alterado

--- raua-marcossoares-gabriel-italo/banco/banco.sql
+++ raua-marcossoares-gabriel-italo/banco/banco.sql
@@ -1,1 +1,8 @@
 create database loja_online;
+
+create table loja_online.produtos (
+    id_produto int primary key,
+    nome varchar(100),
+    descrição varchar(100),
+    id_vendedor int not null 
+);
