9450c23696c1d2a8c15a7fe820c23a425b34457e
AlunoFodido <00001132718521sp@al.educacao.sp.gov.br>
2025-06-03 09:33:16 -0300

    atualização

--- raua-marcossoares-gabriel-italo/banco/banco.sql
+++ raua-marcossoares-gabriel-italo/banco/banco.sql
@@ -1,8 +1,36 @@
 create database loja_online;
 
+create table loja_online.vendedores(
+    id_vendedor int primary key,
+    nome varchar(100),
+    data_cadastro date,
+    email varchar(100) unique,
+    ativo boolean
+
+);
+
+create table loja_online.usuarios(
+    id_usuario int primary key,
+    nome varchar(100),
+    data_cadastro date,
+    email varchar(100) unique,
+    senha varchar(100) unique,
+    ativo boolean
+
+);
+
 create table loja_online.produtos (
     id_produto int primary key,
     nome varchar(100),
     descrição varchar(100),
-    id_vendedor int not null 
+    id_vendedor int not null,
+    data_cadastro date,
+    preco_unitario decimal (10,2) not null,
+    ativo boolean,
+    foreign key (id_vendedor) references vendedores (id_vendedor)
+
 );
+
+select * from loja_online.produtos;
+select * from loja_online.vendedores;
+select * from loja_online.usuarios;
