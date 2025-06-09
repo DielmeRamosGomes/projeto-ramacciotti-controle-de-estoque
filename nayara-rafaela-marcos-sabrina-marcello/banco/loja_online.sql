4311eaef508114264a5597980d893b560e9688aa
wwwspSabrina27 <00001083562435sp@al.educacao.sp.gov.br>
2025-06-09 10:35:33 -0300

    sabrina

--- nayara-rafaela-marcos-sabrina-marcello/banco/loja_online.sql
+++ nayara-rafaela-marcos-sabrina-marcello/banco/loja_online.sql
@@ -1,11 +1,29 @@
 Create database loja_online;
 
-Create table produtos(
-    id_produto int primary key,
-    nome varchar (100),
-    descricao varchar (100),
-    id_vendedor int,
+CREATE TABLE Vendedor (
+    id_vendedor INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
     data_cadastro date,
-    preço_unitario decimal (10,2),
-      foreign key (id_vendedor) references Vendendor (id_vendedor)
+    email VARCHAR(100) NOT NULL UNIQUE,
+    ativo BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE Usuario (
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    data_cadastro DATE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(100) NOT NULL,
+    ativo BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE Produto (
+    id_produto INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    descricao TEXT,
+    id_vendedor INT,
+    data_cadastro DATE,
+    preco_unitario DECIMAL(10, 2) NOT NULL,
+    ativo BOOLEAN DEFAULT TRUE,
+    FOREIGN KEY (id_vendedor) REFERENCES Vendedor(id_vendedor)
 );
