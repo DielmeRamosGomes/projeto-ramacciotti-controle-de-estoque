651d1a2914e29d9a31552ccd15d2f62e8b296c5d
wwwspmarcosvinicius1234 <00001103285828sp@aluno.educacao.sp.gov.br>
2025-06-12 08:50:58 -0300

    att por

---
+++ nayara-rafaela-marcos-sabrina-marcello/back-end/Produto.js
@@ -0,0 +1,18 @@
+
+class Produto {
+
+    constructor(id_produto, nome, descricao, id_vendedor, data_cadastro, preco_unitario, ativo) {
+        this.id_produto = id_produto;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.id_vendedor = id_vendedor;
+        this.data_cadastro = data_cadastro;
+        this.preco_unitario = preco_unitario;
+        this.ativo = ativo;
+    }
+
+}
+export default Produto;
+
+
+
